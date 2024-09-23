652158e32769007da0488dd13080a71df345aa76
Jaspreet Singh <jaspreetbrar1636@gmail.com>
2024-09-23 14:05:29 -0400

    added the skeleton for the routes to different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import {Routes, Route} from 'react-router-dom'
+
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import SignIn from './pages/Signin'
+import SignUp from './pages/Signup'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
 
   return (
     <>
-      <div className="text-blue-500">House Marketplace</div>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
+      {/* Navbar */}
     </>
   )
 }
